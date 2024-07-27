924dc627d83fbada8fa965de5561b5bee1ec4ee6
MohammadSufiyan1 <166398388+MohammadSufiyan1@users.noreply.github.com>
2024-07-27 20:03:13 +0530

    Update remove_bg.py

--- remove_bg.py
+++ remove_bg.py
@@ -1,7 +1,7 @@
 from rembg import remove
 from PIL import Image
-input_img_location = 'C:\\Users\\G.S Technology\\Desktop\\srk.jpg' #input image location (path)
-output_img_location = 'C:\\Users\\G.S Technology\\Desktop\\shahrukh1.png' #ouput image loaction(path) extention name use .png
-input = Image.open(input_img_location) #image open
-output = remove(input)                 #remove background process
-output.save(output_img_location)       #final store data remove background image and check your image output location
+input_img_location = 'C:\\Users\\G.S Technology\\Desktop\\srk.jpg' 
+output_img_location = 'C:\\Users\\G.S Technology\\Desktop\\shahrukh1.png' 
+input = Image.open(input_img_location) 
+output = remove(input)                 
+output.save(output_img_location)      
