2dea7d68a174e5a8e1d0e7248f00b1b426d42382
MohammadSufiyan1 <166398388+MohammadSufiyan1@users.noreply.github.com>
2024-12-03 11:39:39 +0530

    Update remove_bg.py

--- remove_bg.py
+++ remove_bg.py
@@ -1,7 +1,7 @@
 from rembg import remove
 from PIL import Image
-input_img_location = 'C:\\Users\\G.S Technology\\Desktop\\srk.jpg' 
-output_img_location = 'C:\\Users\\G.S Technology\\Desktop\\shahrukh1.png' 
+input_img_location = 'C:\\Users\\G.S Technology\\Desktop\\srk.jpg'         #Input Image Path
+output_img_location = 'C:\\Users\\G.S Technology\\Desktop\\shahrukh1.png'  #Edit the output path and name of the image use .png in the extension name
 input = Image.open(input_img_location) 
 output = remove(input)                 
 output.save(output_img_location)      
